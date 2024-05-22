a7c88545fb6164d4cee1706e3946ed5772f642aa
fady <32818953+FadyDMK@users.noreply.github.com>
2024-05-22 15:07:20 +0200

    started replacing html with js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-function popMessage(){
-    console.log("'ello matey");
+import "./style.css";
+import Image from "./fennec5.jpeg";
+
+function home() {
+  const content = document.getElementById("content");
+  const title = document.createElement("h3");
+  const image = document.createElement("img");
+  title.textContent = "Fennec";
+  image.src = Image;
+  image.alt = "fennec";
+
+  content.appendChild(title);
+  content.appendChild(image);
 }
 
-document.body.appendChild(popMessage());
+function main() {
+  const wrapper = document.createElement('div');
+  wrapper.className = "wrapper";
+
+  const header = document.createElement('header');
+
+  const buttons = ['Home','Menu','About']
+
+
+  wrapper.appendChild(header)
+}
+
+document.body.appendChild(home());
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,41 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-
   entry: {
-    index: './src/index.js',
+    index: "./src/index.js",
   },
   output: {
-
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean:true,
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: './src/index.html',
-    title: "development"
-  })],
-
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "development",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Matches .css files
+        use: [
+          "style-loader", // Injects CSS into the DOM
+          "css-loader", // Turns CSS into JS modules
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|jpeg)$/i, // Matches image files
+        use: [
+          {
+            loader: "file-loader", // Resolves import/require() on a file into a url and emits the file into the output directory
+            options: {
+              name: "[path][name].[ext]", // Keeps the original file name and path
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
